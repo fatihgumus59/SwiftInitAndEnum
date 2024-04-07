c6bfcb3ffbbb4f6e8c52f89894e1caa38aea7ceb
Fatih Gumus <fatihgumus72@gmail.com>
2024-04-08 00:37:54 +0300

    init and enum example project

--- SwiftInitAndEnum/ContentView.swift
+++ SwiftInitAndEnum/ContentView.swift
@@ -9,14 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading,spacing: 10){
+            Home(location: "İstanbul", squareMeter: 100, hasBalcony: true, homeType: .Apartman)
+                .padding(20)
+                .frame(width: 200)
+                .background(.red)
+            
+            
+            Home(location: "Ankara", squareMeter: 120, hasBalcony: false, homeType: .Rezidans)
+                .padding(20)
+                .frame(width: 200)
+                .background(.orange)
+                
         }
-        .padding()
+        .frame(width: 300,height: 300)
+ 
     }
+}
+
+struct Home : View{
+    
+    var location : String
+    var squareMeter : Int
+    var hasBalcony : Bool
+    var homeType : HomeType
+    
+    init(location: String, squareMeter: Int, hasBalcony: Bool, homeType: HomeType) {
+        self.location = location
+        self.squareMeter = squareMeter
+        self.hasBalcony = hasBalcony
+        self.homeType = homeType
+    }
+    
+    
+    var body: some View{
+        VStack(alignment: .leading){
+            Text("Konum :  \(location)")
+            Text("Büyüklük : \(squareMeter)")
+            if hasBalcony{
+                Text("Balkon : Var")
+            }else{
+                Text("Balkon : Yok")
+            }
+            
+            switch homeType{
+                case .Apartman:
+                    Text("Apartman Dairesi")
+                case .Müstakil:
+                    Text("Müstakil Bina")
+                case .Rezidans:
+                    Text("Rezidans")
+            }
+           
+        }
+    }
+}
+
+enum HomeType {
+    case Müstakil
+    case Apartman
+    case Rezidans
 }
 
 #Preview {
