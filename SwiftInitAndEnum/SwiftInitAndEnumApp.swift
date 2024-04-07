b6e48434fb940933b8e5a548ba0de834a6e20bda
Fatih Gumus <fatihgumus72@gmail.com>
2024-04-07 20:40:49 +0300

    Initial Commit

---
+++ SwiftInitAndEnum/SwiftInitAndEnumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftInitAndEnumApp.swift
+//  SwiftInitAndEnum
+//
+//  Created by Fatih Gümüş on 7.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftInitAndEnumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
